533611aec9ee83c22a72bb84f83e6c3d8273b9eb
tomon9086 <tomo.mouse.9086@gmail.com>
2020-04-20 17:00:04 +0900

    add setting modal

---
+++ Assets/Scripts/Modal.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Modal : MonoBehaviour
+{
+    [SerializeField]
+    private Button toggleButton;
+
+    [SerializeField]
+    private GameObject modal;
+    
+    void Start()
+    {
+        if(modal == null)
+        {
+            return;
+        }
+
+        modal.SetActive(false);
+        
+        if(toggleButton != null)
+        {
+            toggleButton.onClick.AddListener(OnClick);
+        }
+    }
+
+    private void OnClick() {
+        bool currVisibility = modal.activeSelf;
+        modal.SetActive(!currVisibility);
+    }
+}
---
+++ Assets/Scripts/SettingsButton.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class SettingsButton : MonoBehaviour
+{
+    private Canvas settingsUI;
+    
+    void Start()
+    {
+        settingsUI = GetComponentInChildren<Canvas>();
+        SetPanelVisibility(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void TogglePanelVisibility() {
+        foreach(Transform child in settingsUI.transform)
+        {
+            bool currVisibility = child.gameObject.activeSelf;
+            child.gameObject.SetActive(!currVisibility);
+        }
+    }
+
+    public void ShowPanel() {
+        SetPanelVisibility(true);
+    }
+
+    public void HidePanel() {
+        SetPanelVisibility(false);
+    }
+
+    private void SetPanelVisibility(bool isVisible) {
+        foreach(Transform child in settingsUI.transform)
+        {
+            child.gameObject.SetActive(isVisible);
+        }
+    }
+}
